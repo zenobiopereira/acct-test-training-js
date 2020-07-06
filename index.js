bcf60b11598848864c246de15db91983d29539ea
Zenobio <zenobio2190@gmail.com>
2020-07-05 23:02:30 -0300

    Mudancas com testes passando

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const button = document.querySelector(".button-test");
-button.innerHTML = "this is a test";
+
 button.addEventListener("click", () => {
-    console.log('test');
+  _toggleButtonClass(button)
+  _toggleButtonText(button)
 });
+
+function _toggleButtonClass(button) {
+  const shouldChangeClass = button.className.includes("test");
+  if(shouldChangeClass) {
+    button.className = "button-clicked";
+  } else {
+    button.className = "button-test";
+  }
+};
+
+function _toggleButtonText(button) {
+  const shouldChangeText = button.textContent.includes("Contact-us");
+  if(shouldChangeText) {
+    button.textContent = "Oh no!";
+  } else {
+    button.textContent = "Contact-us";
+  }
+}
--- index.test.js
+++ index.test.js
@@ -1,10 +1,28 @@
 console.log = jest.fn();
 
-test('Should change button text ', () => {
-    document.body.innerHTML = '<button class="button-test">clique em mim</button>';
+describe("Should change button Properties", () => {
+  beforeAll(() => {
+    document.body.innerHTML = '<button class="button-test">Contact-Us</button>';
     require('./index.js');
+  });
+
+  test('Should toggle button text ', () => {
     const button = document.querySelector(".button-test");
+
     button.click();
-    expect(button.textContent).toBe("this is a test");
-    expect(console.log).toBeCalled();
+    expect(button.textContent).toBe("Oh no!");
+
+    button.click();
+    expect(button.textContent).toBe("Contact-us");
+  });
+
+  test('Should toggle button class', () => {
+    const button = document.querySelector(".button-test");
+
+    button.click();
+    expect(button.className).toBe("button-clicked")
+
+    button.click();
+    expect(button.className).toBe("button-test")
+  });
 })
