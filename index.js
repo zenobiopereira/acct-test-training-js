9bd7e33a1fb58ec579c2f848c871ce2a1d0d87c0
Zenobio <zenobio2190@gmail.com>
2020-07-23 03:32:14 -0300

    Added a mocked API but need to create some validations tests for the mockedApi

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const button = document.querySelector(".button-test");
+const button = document.querySelector(".button-login");
+const tokenInput = document.querySelector(".authentication-box");
+const spanInfo = document.querySelector(".authentication-info");
 
-button.addEventListener("click", () => {
-  _toggleButtonClass(button)
-  _toggleButtonText(button)
+button.addEventListener("click", async () => {
+  _toggleButtonClass(button);
+  _toggleButtonText(button);
+  const result = await authenticate(tokenInput.value);
+  tokenInput.value = "";
+  _validateResponse(result);
+  _toggleButtonClass(button);
+  _toggleButtonText(button);
 });
 
-function _toggleButtonClass(button) {
-  const shouldChangeClass = button.className.includes("test");
-  if(shouldChangeClass) {
-    button.className = "button-clicked";
+function _validateResponse(response) {
+  spanInfo.className = "authentication-info";
+  if (response.code !== 200) {
+    spanInfo.classList.add("error");
   } else {
-    button.className = "button-test";
+    spanInfo.classList.add("success");
   }
-};
+
+  return (spanInfo.innerHTML = response.message);
+}
+
+function _toggleButtonClass(button) {
+  const hasDefaultClass = button.className.includes("login");
+  button.className = hasDefaultClass ? "button-clicked" : "button-login";
+}
 
 function _toggleButtonText(button) {
-  const shouldChangeText = button.textContent.includes("Contact-us");
-  if(shouldChangeText) {
-    button.textContent = "Oh no!";
-  } else {
-    button.textContent = "Contact-us";
-  }
+  const hasDefaultText = button.textContent.includes("Authenticate");
+  button.textContent = hasDefaultText ? "Authenticating..." : "Authenticate";
 }
